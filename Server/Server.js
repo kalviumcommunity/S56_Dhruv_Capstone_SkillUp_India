1791c28816c89c565ca0228d3df4507ed3151ff0
Dhruv Patil <138751440+Dhruv-Patil-1720@users.noreply.github.com>
2024-04-04 10:48:36 +0530

    Update Server.js
    made changes according to review

--- Server/Server.js
+++ Server/Server.js
@@ -4,16 +4,26 @@ require('dotenv').config();
 const port = process.env.PORT || 3000; 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  }).then(() => {
+// Middleware for parsing JSON bodies
+app.use(express.json());
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
-  }).catch((error) => {
+  } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1);
-  });
-  
-app.get('/', (req, res) => {
+  }
+}
+connectToDB();
+app.get('/', async (req, res) => {
+  try {
     res.send('<h1>Hello, Future World!</h1>');
+  } catch (error) {
+    console.error('Error handling request:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(port, () => {
